7a8b84e36c2564691bb7e89ede437f23b1bf5098
WaffleSquirrel <rockabilly@outlook.com>
2015-09-20 03:10:26 -0700

    Hooking up SignalR and adding a Connection broadcaster hub in the Website project.

---
+++ src/1. Clients/Web/dependAnt.Website/Infrastructure/Communication/Hubs/ConnectionBroadcaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace dependAnt.Website.Infrastructure.Communication.Hubs
+{
+    public class ConnectionBroadcaster : Hub
+    {
+        public override Task OnConnected()
+        {
+            const string broadcastMessageFormatString = "[NEW Connection] => Connection ID {0}";
+
+            return base.Clients.All.Message(string.Format(broadcastMessageFormatString, Context.ConnectionId));
+        }
+
+        public Task Broadcast(string message)
+        {
+            const string broadcastMessageFormatString = "[{0}] => {1}";
+
+            return base.Clients.All.Message(string.Format(broadcastMessageFormatString, Context.ConnectionId, message));
+        }
+    }
+}
