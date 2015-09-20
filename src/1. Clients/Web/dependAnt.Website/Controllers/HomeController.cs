6a748706eb40709bc3c9d9dc1989cd007eb4d4ba
WaffleSquirrel <rockabilly@outlook.com>
2015-09-20 03:18:36 -0700

    Adding a base Ant Controller for MVC in the Website project.

--- src/1. Clients/Web/dependAnt.Website/Controllers/HomeController.cs
+++ src/1. Clients/Web/dependAnt.Website/Controllers/HomeController.cs
@@ -1,11 +1,22 @@
 ﻿using System.Web.Mvc;
+using dependAnt.Website.Infrastructure.Communication.Hubs;
+using dependAnt.Website.Infrastructure.Web;
 
 namespace dependAnt.Website.Controllers
 {
-    public class HomeController : Controller
+    /// <summary>
+    /// The home MVC controller for the Web API UI.
+    /// </summary>
+    public class HomeController : BaseAntController<ConnectionBroadcaster>
     {
+        /// <summary>
+        /// Returns the default view for displaying the Home Page.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
+            ViewBag.Title = "Send me on my way.";
+
             return View();
         }
     }
---
+++ src/1. Clients/Web/dependAnt.Website/Infrastructure/Web/BaseAntController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace dependAnt.Website.Infrastructure.Web
+{
+    public abstract class BaseAntController<THub> : Controller where THub : Hub
+    {
+        public IHubConnectionContext<dynamic> Clients { get; private set; }
+
+        public IGroupManager Groups { get; private set; }
+
+        protected BaseAntController()
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<THub>();
+
+            this.Clients = hubContext.Clients;
+            this.Groups = hubContext.Groups;
+        }
+    }
+}
