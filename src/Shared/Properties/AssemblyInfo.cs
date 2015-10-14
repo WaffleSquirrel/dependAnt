0a3c22fa0ce0d3135e54e8a470ab78460998ad86
WaffleSquirrel <rockabilly@outlook.com>
2015-10-14 00:26:55 -0700

    Adding AssemblyInfo to dependAnt.Core PCL project.

---
+++ src/Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dependAnt.Core (PCL)")]
+[assembly: AssemblyCopyright("Copyright © Steve Mickle 2015")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("260f5a9b-d255-43ff-a460-2e5f215805e2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
