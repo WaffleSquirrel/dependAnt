83339c99d3179145c6ddf158bd997dacfb73ab5f
WaffleSquirrel <rockabilly@outlook.com>
2015-10-16 15:23:51 -0700

    Adding some extensions classes and supporting monitoring infrastructure.

---
+++ src/Shared/dependAnt.Shared/Monitoring/IHeartbeat.cs
@@ -0,0 +1,8 @@
+﻿namespace dependAnt.Core.Monitoring
+{
+    public interface IHeartbeat<out T>
+    {
+        bool IsHeartbeat { get; }
+        T Update { get; }
+    }
+}
---
+++ src/Shared/dependAnt.Shared/Monitoring/IStale.cs
@@ -0,0 +1,8 @@
+﻿namespace dependAnt.Core.Monitoring
+{
+    public interface IStale<out T>
+    {
+        bool IsStale { get; }
+        T Update { get; }
+    }
+}
