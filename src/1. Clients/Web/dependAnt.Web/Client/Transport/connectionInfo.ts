dce8dcc8f7486468fe9dedd24ac2967165d888dc
WaffleSquirrel <rockabilly@outlook.com>
2015-11-06 20:50:28 -0800

    Adding ConnectionInfo TypeScript class in Web project.

---
+++ src/1. Clients/Web/dependAnt.Web/Client/Transport/connectionInfo.ts
@@ -0,0 +1,15 @@
+﻿class ConnectionInfo {
+    connectionStatus: ConnectionStatus;
+    connectionType: ConnectionType;
+    server: string;
+
+    constructor(connectionStatus: ConnectionStatus, connectionType: ConnectionType, server: string) {
+        this.connectionStatus = connectionStatus;
+        this.connectionType = connectionType;
+        this.server = server;
+    }
+
+    toString() {
+        return "ConnectionStatus: " + this.connectionStatus + ", Type: " + this.connectionType + ", Server: " + this.server;
+    }
+}
