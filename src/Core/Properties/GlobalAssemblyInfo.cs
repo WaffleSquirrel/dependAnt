adba551a6ab331b025fe1796300da4a51149028e
WaffleSquirrel <rockabilly@outlook.com>
2016-01-22 18:09:04 -0800

    Adding a GlobalAssemblyInfo file.

---
+++ src/Core/Properties/GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct("dependAnt.Core")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright (c) 2015")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
